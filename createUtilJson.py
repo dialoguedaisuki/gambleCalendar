3a16c7b28e9cb1a817be4ccebad15dba19a46c2a
mo mo <mailhasimasen@nomail.com>
2022-06-22 09:00:45 +0900

    地域区分関数完成

--- createUtilJson.py
+++ createUtilJson.py
@@ -13,17 +13,8 @@ sikoku = ['徳島県', '香川県', '愛媛県', '高知県']
 kyushu = ['福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
 
 
-def jsonLoad(jsonName):
-    loadJson = json.load(open(f'{jsonName}.json'))
-    return loadJson
-
-
-def twoDimeLs(ls):
-    return list(map(list, set(map(tuple, ls))))
-
-
 def judgeTikiKubun(jyo, ken):
-    if jyo == "二ヶ村緑道":
+    if ken in "二ヶ村緑道":
         return ['関東', "東京", jyo]
     if ken in hokkaido:
         return ["北海道", ken, jyo]
@@ -46,8 +37,16 @@ def judgeTikiKubun(jyo, ken):
     if ken in kyushu:
         return ["九州", ken, jyo]
 
-    # 競艇
-    # ['多摩川', '二ヶ村緑道']
+
+def jsonLoad(jsonName):
+    loadJson = json.load(open(f'{jsonName}.json'))
+    return loadJson
+
+
+def twoDimeLs(ls):
+    return list(map(list, set(map(tuple, ls))))
+
+
 tyohukuLs = []
 cityDict = {}
 shubetu = ['keirin', 'autorace', 'keiba', 'kyotei']
@@ -61,7 +60,10 @@ for s in shubetu:
 tyohukuLsSet = twoDimeLs(tyohukuLs)
 # pprint(tyohukuLsSet)
 for i, j in tyohukuLsSet:
-    print(i, j)
     cityDict['ken'] = j
     cityDict['jyo'] = i
-    print(judgeTikiKubun(i, j))
+    result = judgeTikiKubun(i, j)
+    if result == None:
+        print("none")
+        break
+    print(result)
